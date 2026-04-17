800686c4163a60a9e638d74ce5278ac972667ffd
Messlow <github.pummel909@passmail.net>
2026-04-17 10:43:51 -0400

    Add layers

--- keyboards/sofle_choc/keymaps/Messlow/keymap.c
+++ keyboards/sofle_choc/keymaps/Messlow/keymap.c
@@ -14,170 +14,103 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Right now the symbols are messed up because it assumes an english layout. Seems like this does not work.
 #include "keymap_canadian_french.h"
 #include QMK_KEYBOARD_H
 
-// TODO: Add dead keys FR ([], {} <>, ç, `, é)
 enum layer_names {
-    // BASE_QWERTY_LAYER,
     BASE_COLEMAK_LAYER,
     NAVIGATION_LAYER,
     SYMBOL_LAYER,
-    FUNCTION_LAYER,
-    MOUSE_LAYER,
 };
 
-// clang-format off
+// TODO: missing keys ç ¨ `
+// TODO: fill thumb clusters (probably shortcuts or OSM keys or toggles for additional shortcut layers)
+// TODO: consider removing home row mods in favor of tap toggles with OSM keys in dedicated layer?
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * QWERTY
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
-     * |------+------+------+------+------+------|  Mute |    | Pause |------+------+------+------+------+------|
-     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
-     * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *            | LCTL | LGUI | LCMD | LALT | /Enter  /       \Space \  | RALT | RCMD | RGUI | RCTL |
-     *            |      |      |      |      |/       /         \      \ |      |      |      |      |
-     *            `----------------------------------'           '------''---------------------------'
-     */
-
-    // [BASE_QWERTY_LAYER] = LAYOUT(
-    //     KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_RGUI,
-    //     KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
-    //     KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    //     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,   MS_BTN3,KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    //                     KC_LCTL, KC_LALT, TT(NAVIGATION_LAYER), KC_ENT, KC_DEL,    KC_BSPC, KC_SPC,  NAV_ESC,  KC_RALT, KC_RCTL
-    // ),
-
-    // /*
-    //  * COLEMAK-DH
-    //  * ,-----------------------------------------.                    ,-----------------------------------------.
-    //  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
-    //  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    //  * | ESC  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
-    //  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    //  * | Tab  |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |  '   |
-    //  * |------+------+------+------+------+------|  Mute |    | Pause |------+------+------+------+------+------|
-    //  * |LShift|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |RShift|
-    //  * `-----------------------------------------/       /     \      \-----------------------------------------'
-    //  *            | LCTL | LGUI | LCMD | LALT | /Enter  /       \Space \  | RALT | RCMD | RGUI | RCTL |
-    //  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
-    //  *            `----------------------------------'           '------''---------------------------'
-    //  */
     [BASE_COLEMAK_LAYER] = LAYOUT(
-        KC_GRV,   KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                          KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_RGUI,
-        KC_ESC,   KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,                          KC_J,     KC_L,     KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        KC_TAB,   LALT_T(KC_A), LCTL_T(KC_R), LSFT_T(KC_S), LGUI_T(KC_T), KC_G,                          KC_M, RGUI_T(KC_N), RSFT_T(KC_E), RCTL_T(KC_I), RALT_T(KC_O), KC_QUOT,
-        KC_LSFT,  KC_Z,         KC_X,         KC_C,         KC_D,         KC_V, KC_MPLY,        MS_BTN3, KC_K,     KC_H,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LCTL, KC_LALT, LT(NAVIGATION_LAYER, KC_ESC), KC_ENT, KC_DEL,    KC_BSPC, LT(SYMBOL_LAYER, KC_SPC),  LT(FUNCTION_LAYER, KC_ESC),  KC_RALT, KC_RCTL
-    ),
+        // Left number rows
+        FR_HASH, KC_1, KC_2, KC_3, KC_4, KC_5,
+        // Right number rows
+        KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
 
-    /*
-     * NAVIGATION
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |______|______|______|______|______|______|                    |______|______|______|______|______|______|
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |______|______|______|______|______|______|                    |______|______|______|______|______|______|
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |______|______|______|______|______|______|-------.    ,-------|______|   <  |   ∨  |   ∧  |   >  |______|
-     * |------+------+------+------+------+------|______ |    |______ |------+------+------+------+------+------|
-     * |______|______|______|______|______|______|-------|    |-------|______|______|______|______|______|______|
-     * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *            |______|______|______|______| /______ /       \______\  |______|______|______|______|
-     *            |      |      |      |      |/       /         \      \ |      |      |      |      |
-     *            `----------------------------------'           '------''---------------------------'
-     */
+        // Left top row
+        KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B,
+        // Right top row
+        KC_J, KC_L, KC_U, KC_Y, FR_SCLN, FR_DCIR,
+
+        // Left home row
+        KC_NO, LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G,
+        // Right home row
+        KC_M, RCTL_T(KC_N), RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), FR_SCLN,
+
+        // Left bottom row + encoder
+        KC_NO, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_MPLY,
+        // Right bottom row + encoder
+        MS_BTN3, KC_K, KC_H, KC_COMM, KC_DOT, FR_EACU, FR_DGRV,
+
+        // Left thumb cluster
+        KC_NO, KC_NO, KC_ESC, LT(NAVIGATION_LAYER, KC_SPC), KC_TAB,
+        // Right thumb cluster
+        KC_ENT, LT(SYMBOL_LAYER, KC_BSPC), KC_DEL, KC_NO, KC_NO),
+
     [NAVIGATION_LAYER] = LAYOUT(
-         KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_EXLM,  KC_EQL,   KC_LT,   KC_GT, KC_MINUS,                        KC_AGIN, KC_PSTE,  KC_COPY,  KC_CUT,  KC_UNDO, KC_BSPC,
-        _______, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_PIPE,                         _______, KC_LEFT,  KC_DOWN,   KC_UP, KC_RIGHT,  KC_DEL,
-        _______, _______, KC_HASH, KC_LCBR, KC_RCBR, KC_PLUS, _______,       _______,  KC_INS, KC_HOME,  KC_PGDN, KC_PGUP,   KC_END, _______,
-                             _______, _______, _______, _______, _______,  _______, _______, _______,  _______, _______
-    ),
+        // Left number rows
+        KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
+        // Right number rows
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
 
-    /*
-     * SYMBOL
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |      |      |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |   1  |   2  |   3  |   4  |   5  |-------.    ,-------|   6  |   7  |   8  |   9  |   0  |      |
-     * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-     * |      |   -  |   =  |   [  |   ]  |   \  |-------|    |-------|      |      |      |      |      |      |
-     * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *            |      |      |      |      | /       /       \      \  |      |      |      |      |
-     *            |      |      |      |      |/       /         \      \ |      |      |      |      |
-     *            `----------------------------------'           '------''---------------------------'
-     */
+        // Left top row
+        KC_NO, OSM(MOD_HYPR), OSM(MOD_LCTL | MOD_LALT), OSM(MOD_LCTL | MOD_LSFT), OSM(MOD_LCTL | MOD_LGUI), KC_NO,
+        // Right top row
+        KC_AGIN, KC_PSTE, KC_COPY, KC_CUT, KC_UNDO, KC_NO,
+
+        // Left home row
+        KC_NO, KC_LEFT_GUI, KC_LEFT_ALT, KC_LEFT_SHIFT, KC_LEFT_CTRL, KC_NO,
+        // Right home row
+        KC_CAPS_LOCK, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO,
+
+        // Left bottom row + encoder
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _______,
+        // Right bottom row + encoder
+        _______, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO,
+
+        // Left thumb cluster
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        // Right thumb cluster
+        _______, _______, _______, _______, _______),
+
     [SYMBOL_LAYER] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______,       _______, _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______, _______, _______,  _______, _______, _______,  _______, _______
-    ),
+        // Left number rows
+        _______, _______, _______, _______, _______, _______,
+        // Right number rows
+        _______, _______, _______, _______, _______, _______,
 
-    /*
-     * FUNCTION
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |      |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |  F10 | F11  | F12  |      |      |-------.    ,-------|      |      |      |      |      |      |
-     * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-     * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
-     * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *            |      |      |      |      | /       /       \      \  |      |      |      |      |
-     *            |      |      |      |      |/       /         \      \ |      |      |      |      |
-     *            `----------------------------------'           '------''---------------------------'
-     */
-    [FUNCTION_LAYER] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   _______, _______,
-        _______, KC_F10,  KC_F11,  KC_F12,  _______, _______,                      _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______, _______, _______,  _______, _______, _______,  _______, _______
-    ),
+        // Left top row
+        KC_NO, FR_TILD, FR_BSLS, FR_SLSH, FR_PIPE, FR_DLR,
+        // Right top row
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
-    /*
-     * MOUSE
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |      |      |      |      |      |                    |      | WH_L | WH_D | WH_U | WH_R |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |      |      |      |      |      |-------.    ,-------|      | MS_L | MS_D | MS_U | MS_R |      |
-     * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-     * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
-     * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *            |      |      |      | BTN1 | / BTN2 /       \ BTN3 \  |      |      |      |      |
-     *            |      |      |      |      |/       /         \      \ |      |      |      |      |
-     *            `----------------------------------'           '------''---------------------------'
-     */
-    [MOUSE_LAYER] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                      _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
-        _______, _______, _______, _______, _______, _______,                      _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
-        _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______, KC_BTN1, KC_BTN2,  KC_BTN3, _______, _______,  _______, _______
-    )
+        // Left home row
+        KC_NO, FR_LABK, FR_LBRC, FR_LBRC, FR_LPRN, FR_PERC,
+        // Right home row
+        KC_NO, FR_RPRN, FR_RBRC, FR_RBRC, FR_RABK, KC_NO,
 
+        // Left bottom row + encoder
+        KC_NO, FR_ASTR, FR_MINS, FR_PLUS, FR_EQL, FR_AMPR, _______,
+        // Right bottom row + encoder
+        _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+        // Left thumb cluster
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        // Right thumb cluster
+        _______, _______, _______, _______, _______),
 };
-// clang-format on
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    // [BASE_QWERTY_LAYER] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
     [BASE_COLEMAK_LAYER] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
     [NAVIGATION_LAYER]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
-    [TRANSPARENT_LAYER]  = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
+    [SYMBOL_LAYER]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
 };
 #endif
 
