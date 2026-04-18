b4de60292bb25f253de7ee783240e396938948d3
Messlow <github.pummel909@passmail.net>
2026-04-18 09:00:35 -0400

    Add missing keys

--- keyboards/sofle_choc/keymaps/Messlow/keymap.c
+++ keyboards/sofle_choc/keymaps/Messlow/keymap.c
@@ -23,35 +23,34 @@ enum layer_names {
     SYMBOL_LAYER,
 };
 
-// TODO: missing keys ç ¨ `
-// TODO: fill thumb clusters (probably shortcuts or OSM keys or toggles for additional shortcut layers)
 // TODO: consider removing home row mods in favor of tap toggles with OSM keys in dedicated layer?
+// TODO: rework thumb clusters (probably shortcuts or OSM keys or toggles for additional shortcut layers)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_COLEMAK_LAYER] = LAYOUT(
         // Left number rows
         FR_HASH, KC_1, KC_2, KC_3, KC_4, KC_5,
         // Right number rows
-        KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
+        KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
 
         // Left top row
-        KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B,
+        KC_CAPS_LOCK, KC_Q, KC_W, KC_F, KC_P, KC_B,
         // Right top row
-        KC_J, KC_L, KC_U, KC_Y, FR_SCLN, FR_DCIR,
+        KC_J, KC_L, KC_U, KC_Y, FR_DCIR, FR_CEDL,
 
         // Left home row
-        KC_NO, LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G,
+        OSM(MOD_LSFT), LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G,
         // Right home row
         KC_M, RCTL_T(KC_N), RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), FR_SCLN,
 
         // Left bottom row + encoder
-        KC_NO, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_MPLY,
+        OSM(MOD_LCTL), KC_Z, KC_X, KC_C, KC_D, KC_V, KC_MPLY,
         // Right bottom row + encoder
         MS_BTN3, KC_K, KC_H, KC_COMM, KC_DOT, FR_EACU, FR_DGRV,
 
         // Left thumb cluster
-        KC_NO, KC_NO, KC_ESC, LT(NAVIGATION_LAYER, KC_SPC), KC_TAB,
+        KC_LALT, KC_LCTL, KC_ESC, LT(NAVIGATION_LAYER, KC_SPC), KC_TAB,
         // Right thumb cluster
-        KC_ENT, LT(SYMBOL_LAYER, KC_BSPC), KC_DEL, KC_NO, KC_NO),
+        KC_ENT, LT(SYMBOL_LAYER, KC_BSPC), KC_DEL, KC_RSFT, KC_RGUI),
 
     [NAVIGATION_LAYER] = LAYOUT(
         // Left number rows
